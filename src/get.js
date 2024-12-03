171e68779eb364778b95540c563d3ef8e49cdc8f
zablon <zablonoigo51@gmail.com>
2024-12-03 08:10:26 +0300

    Add a method to list files

---
+++ src/get.js
@@ -0,0 +1,12 @@
+const AWS=require("aws-sdk")
+const s3= new AWS.S3();
+
+const BUCKET_NAME=process.env.FILE_UPLOAD_BUCKET_NAME
+
+module.exports.handler=async(event)=>{
+    console.log(event);
+    const response={
+        isBase64Encoded: false,
+        statusCode: 200,
+    };
+}
