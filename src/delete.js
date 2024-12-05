92f77dee4c3158ceed5c8234b5fae3da2f31b296
zablon <zablonoigo51@gmail.com>
2024-12-05 07:18:02 +0300

    Add response status code

--- src/delete.js
+++ src/delete.js
@@ -4,4 +4,8 @@ const s3= new AWS.S3()
 const BUCKET_NAME=process.env.FILE_UPLOAD_BUCKET_NAME
 module.exports.handler=async(event)=>{
     console.log(event)
+    const response={
+        isBase64Encoded: false,
+        statusCode: 200,
+    };
 }
